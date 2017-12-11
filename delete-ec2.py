536c030e521810cfa0acfbda84b86ad4772554c7
Himanshu Sachdeva <sachdeva.h@gmail.com>
2017-12-11 15:23:46 +0530

    deleted creds

--- delete-ec2.py
+++ delete-ec2.py
@@ -1,5 +1,5 @@
 import boto.ec2
-conn = boto.ec2.connect_to_region("us-east-1", aws_access_key_id='AKIAIH6WXO2W3Y2YTI7A', aws_secret_access_key='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')
+conn = boto.ec2.connect_to_region("us-east-1", aws_access_key_id='', aws_secret_access_key='')
 
 
 conn.terminate_instances('i-08fa86c9baded6e1b')
